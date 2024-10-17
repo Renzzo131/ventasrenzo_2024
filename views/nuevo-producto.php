0c6cb3ef57de264296563a22c3fb2d837c5fa188
Renzzo131 <renzoeduardo1304@gmail.com>
2024-10-17 09:21:30 -0500

    cambios
    ultimos cambios

--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -8,31 +8,34 @@
         <input type="text" class="form-control" id="nombre" name="nombre" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="detalle" class="form-label">Detalle:</label>
+        <input type="text" class="form-control" id="detalle" name="detalle" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="precio" class="form-label">Precio:</label>
+        <input type="text" class="form-control" id="precio" name="precio" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="stock" class="form-label">Stock:</label>
+        <input type="text" class="form-control" id="stock" name="stock" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="imagen1" class="form-label">Imagen 1:</label>
+        <input type="file" class="form-control-file" id="imagen1" name="imagen1" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="imagen2" class="form-label">Imagen 2:</label>
+        <input type="file" class="form-control-file" id="imagen2" name="imagen2" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="imagen3" class="form-label">Imagen 3:</label>
+        <input type="file" class="form-control-file" id="imagen3" name="imagen3" required>
     </div>
     <div class="col-12">
-        <label for="codigo" class="form-label">Código:</label>
-        <input type="text" class="form-control" id="codigo" name="codigo" required>
+        <label for="imagen4" class="form-label">Imagen 4:</label>
+        <input type="file" class="form-control-file" id="imagen4" name="imagen4" required>
+    </div>
+    <div class="col-12">
+        <button type="submit" class="btn btn-primary"></button>
     </div>
 </form>
