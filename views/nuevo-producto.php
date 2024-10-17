e68212a77314aaeb4a677328f1bec7ff456e53fa
Renzzo131 <renzoeduardo1304@gmail.com>
2024-10-17 11:21:24 -0500

    borrado en css

--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -1,45 +1,45 @@
-<form action="" method="" id="formul">
-    <div class="col-12">
+<form action="" method="">
+    <div class="col-12 e-form">
         <label for="codigo" class="form-label">Código:</label>
         <input type="text" class="form-control" id="codigo" name="codigo" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="Nombre" class="form-label">Nombre:</label>
         <input type="text" class="form-control" id="nombre" name="nombre" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="detalle" class="form-label">Detalle:</label>
         <input type="text" class="form-control" id="detalle" name="detalle" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="precio" class="form-label">Precio:</label>
         <input type="number" class="form-control" id="precio" name="precio" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="stock" class="form-label">Stock:</label>
         <input type="number" class="form-control" id="stock" name="stock" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="categoria" class="form-label">Stock:</label>
         <input type="number" class="form-control" id="categoria" name="categoria" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="imagen1" class="form-label">Imagen 1:</label>
         <input type="text" class="form-control" id="imagen1" name="imagen1" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="imagen2" class="form-label">Imagen 2:</label>
         <input type="text" class="form-control" id="imagen2" name="imagen2" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="imagen3" class="form-label">Imagen 3:</label>
         <input type="text" class="form-control" id="imagen3" name="imagen3" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <label for="imagen4" class="form-label">Imagen 4:</label>
         <input type="text" class="form-control" id="imagen4" name="imagen4" required>
     </div>
-    <div class="col-12">
+    <div class="col-12 e-form">
         <button type="reset" class="btn btn-danger">Cancelar</button>
         <button type="submit" class="btn btn-primary">Registrar</button>
     </div>
