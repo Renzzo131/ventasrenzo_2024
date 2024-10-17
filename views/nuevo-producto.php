8cde2a88ad16cbeefbc87b844604ed754731e780
Renzzo131 <renzoeduardo1304@gmail.com>
2024-10-17 10:05:38 -0500

    cambiaso
    cambios finos

--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -13,29 +13,34 @@
     </div>
     <div class="col-12">
         <label for="precio" class="form-label">Precio:</label>
-        <input type="text" class="form-control" id="precio" name="precio" required>
+        <input type="number" class="form-control" id="precio" name="precio" required>
     </div>
     <div class="col-12">
         <label for="stock" class="form-label">Stock:</label>
-        <input type="text" class="form-control" id="stock" name="stock" required>
+        <input type="number" class="form-control" id="stock" name="stock" required>
+    </div>
+    <div class="col-12">
+        <label for="categoria" class="form-label">Stock:</label>
+        <input type="number" class="form-control" id="categoria" name="categoria" required>
     </div>
     <div class="col-12">
         <label for="imagen1" class="form-label">Imagen 1:</label>
-        <input type="file" class="form-control-file" id="imagen1" name="imagen1" required>
+        <input type="text" class="form-control" id="imagen1" name="imagen1" required>
     </div>
     <div class="col-12">
         <label for="imagen2" class="form-label">Imagen 2:</label>
-        <input type="file" class="form-control-file" id="imagen2" name="imagen2" required>
+        <input type="text" class="form-control" id="imagen2" name="imagen2" required>
     </div>
     <div class="col-12">
         <label for="imagen3" class="form-label">Imagen 3:</label>
-        <input type="file" class="form-control-file" id="imagen3" name="imagen3" required>
+        <input type="text" class="form-control" id="imagen3" name="imagen3" required>
     </div>
     <div class="col-12">
         <label for="imagen4" class="form-label">Imagen 4:</label>
-        <input type="file" class="form-control-file" id="imagen4" name="imagen4" required>
+        <input type="text" class="form-control" id="imagen4" name="imagen4" required>
     </div>
     <div class="col-12">
-        <button type="submit" class="btn btn-primary"></button>
+        <button type="reset" class="btn btn-danger">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Registrar</button>
     </div>
 </form>
