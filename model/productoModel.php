e46f397194eeb810c39f06ecc8813054f725d889
Renzzo131 <renzoeduardo1304@gmail.com>
2024-10-22 09:33:38 -0500

    mandando a la base de datos
    conexion con la base de datos

--- model/productoModel.php
+++ model/productoModel.php
@@ -8,7 +8,7 @@ class ProductoModel{
     }
 
     public function resgistrarProducto($codigo, $nombre, $detalle, $precio, $stock, $categoria, $imagen1, $imagen2, $imagen3, $imagen4, $proveedor){
-
+        $sql = $this->conexion->query("CALL insertproducto('{$codigo}','{$nombre}','{$detalle}','{$precio}','{$stock}','{$categoria}','{$imagen1}','{$imagen2}','{$imagen3}','{$imagen4}','{$proveedor}')");
     }
 }
 
