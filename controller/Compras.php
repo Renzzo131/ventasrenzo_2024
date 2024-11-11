893c4493a4dc132f40d12c68eaa85248c2bf277d
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-11 12:52:28 -0500

    functions_compras
    queda

--- controller/Compras.php
+++ controller/Compras.php
@@ -28,21 +28,24 @@ require_once "../model/categoriaModel.php";
 $tipo = $_REQUEST['tipo'];
 
 //instancia de la clase ProductoModel
-$objCategoria = new ComprasModel();
+$objCompras = new ComprasModel();
 if ($tipo == "registrar") {
     //print_r($_POST);
     //echo $_FILES['imagen1']['name'];
         if ($_POST) {
-        $nombre = $_POST['nombre'];
-        $detalle = $_POST['detalle'];
-        if ($nombre == "" || $detalle == "") {
+        $id_producto = $_POST['nombre'];
+        $cantidad = $_POST['detalle'];
+        $precio = $_POST['detalle'];
+        $fecha_compra = $_POST['detalle'];
+        $id_persona = $_POST['detalle'];
+        if ($id_producto == "" || $cantidad == ""|| $precio == ""|| $fecha_compra == ""|| $id_persona == "") {
             //Vamos a responder con una estructura de objetos en formato JSON
             //RESPUESTA
             $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error, campos vacíos');
         }else{
-            $arrPersona = $objCategoria->resgistrarCompras($id_producto, $cantidad, $precio, $fecha_compra, $id_persona);
+            $arrCompras = $objCompras->resgistrarCompras($id_producto, $cantidad, $precio, $fecha_compra, $id_persona);
             //id es lo que me devuelve la base de datos por el procedimiento
-            if ($arrPersona->id > 0) {
+            if ($arrCompras->id > 0) {
                 $arr_Respuesta = array('status'=>true, 'mensaje'=>'Registro exitoso');
             }else{
                 $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error al registrar producto');
