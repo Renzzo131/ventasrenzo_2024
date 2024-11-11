27cac56af0cac52b954874f6c5d8e42d7cc1ac0a
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-11 09:57:52 -0500

    controlador persona
    modificación al codigo del controlador de persona

--- controller/Persona.php
+++ controller/Persona.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../model/productoModel.php";
+require_once "../model/personaModel.php";
 $tipo = $_REQUEST['tipo'];
 
 //instancia de la clase ProductoModel
@@ -8,39 +8,29 @@ if ($tipo == "registrar") {
     //print_r($_POST);
     //echo $_FILES['imagen1']['name'];
         if ($_POST) {
-        $codigo = $_POST['codigo'];
-        $nombre = $_POST['nombre'];
-        $detalle = $_POST['detalle'];
-        $precio = $_POST['precio'];
-        $stock = $_POST['stock'];
-        $imagen_1 = 'imagen1';
-        $id_categoria = $_POST['categoria'];
-        $id_proveedor = $_POST['proveedor'];
-        if ($codigo == "" || $nombre == "" || $detalle == "" || $precio == "" || $stock == "" || $imagen_1 == "" || $id_categoria == "" || $id_proveedor == "") {
+        $nro_identidad = $_POST['nro_identidad'];
+        $razon_social = $_POST['razon_social'];
+        $telefono = $_POST['telefono'];
+        $departamento = $_POST['departamento'];
+        $provincia = $_POST['provincia'];
+        $distrito = 'distrito';
+        $cod_postal = $_POST['cod_postal'];
+        $direccion = $_POST['direccion'];
+        $rol = $_POST['rol'];
+        $correo = $_POST['correo'];
+        $password = $_POST['password'];
+        $estado = $_POST['estado'];
+        if ($nro_identidad == "" || $razon_social == "" || $telefono == "" || $departamento == "" || $provincia == "" || $distrito == "" || $cod_postal == "" || $direccion == ""|| $rol == ""|| $correo == ""|| $password == ""|| $estado == "") {
             //Vamos a responder con una estructura de objetos en formato JSON
             //RESPUESTA
             $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error, campos vacíos');
         }else{
-            $arrProducto = $objProducto->resgistrarProducto(
-                $codigo, $nombre, $detalle, $precio, $stock,  $imagen_1, $id_categoria, $id_proveedor
+            $arrPersona = $objProducto->resgistrarProducto(
+                $nro_identidad, $razon_social, $telefono, $departamento, $provincia,  $distrito, $cod_postal, $direccion, $rol, $correo, $password, $estado
             );
             //id es lo que me devuelve la base de datos por el procedimiento
-            if ($arrProducto->id > 0) {
+            if ($arrPersona->id > 0) {
                 $arr_Respuesta = array('status'=>true, 'mensaje'=>'Registro exitoso');
-                //Xargar archivos
-                $archivo = $_FILES['imagen1']['tmp_name'];
-                $destino = '/assets/img_productos/';
-                //strtolower convierte todo el texto a minusculas
-                $tipoArchivo =  strtolower(pathinfo($_FILES["imagen1"]["name"],PATHINFO_EXTENSION));
-
-                $nombre = $arrProducto->id.".".$tipoArchivo;
-
-                if (move_uploaded_file($archivo,$destino.$nombre)) {
-                    $arr_imagen1 = $objProducto->actualizar_imagen($id,$nombre);
-                }else{
-                    $arr_Respuesta = array('status' => true, 'mensaje' => 'Registro exitoso, pero error al subir la imagen');
-                }
-
             }else{
                 $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error al registrar producto');
             }
