f15a982d5852a20330a744317ab9e4cb65b757b0
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-11 09:34:14 -0500

    realizado el model de persona
    Codigo

---
+++ model/personaModel.php
@@ -0,0 +1,25 @@
+<?php
+require_once "../librerias/conexion.php";
+class PersonaModel
+{
+    private $conexion;
+    function __construct()
+    {
+        $this->conexion = new Conexion();
+        $this->conexion = $this->conexion->connect();
+    }
+
+    public function resgistrarPersona($nro_identidad, $razon_social, $telefono, $departamento, $provincia, $distrito, $cod_postal, $direccion, $rol, $correo, $password, $estado)
+    {
+        $sql = $this->conexion->query("CALL registrar_persona('{$nro_identidad}','{$razon_social}','{$telefono}','{$departamento}','{$provincia}','{$distrito}','{$cod_postal}','{$direccion}','{$rol}','{$correo}','{$password}','{$estado}')");
+        //$sql = $this->conexion->query("select * from producto");
+        if ($sql === false) {
+            // Mostrar el error de la consulta
+            die("Error en la consulta: " . $this->conexion->error);
+        }
+        $sql = $sql->fetch_object();
+        return $sql;
+    }
+}
+
+    
