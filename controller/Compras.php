a489b9f217a94ee08883fa104dc55bca7f60bcd1
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-11 12:49:30 -0500

    trabajando compras

---
+++ controller/Compras.php
@@ -0,0 +1,55 @@
+<?php
+require_once "../model/categoriaModel.php";
+
+$tipo = $_REQUEST['tipo'];
+
+$objCategoria = new categoriaModel();
+
+if ($tipo == "listar"){
+    //Respuesta
+    $arr_Respuesta = array('status'=>false, 'contenido'=>'');
+    $arrCategorias = $objCategoria->obtener_categoria();
+    if (!empty($arrCategorias)){
+       //Recorremos el array para agregar las opciones de las categorias
+        for ($i=0; $i < count($arrCategorias); $i++) { 
+            $id_categoria = $arrCategorias[$i]->id;
+            $categoria = $arrCategorias[$i]->nombre;
+            $opciones = '';
+            $arrCategorias[$i]->options = $opciones;
+       } 
+       $arr_Respuesta['status'] = true;
+       $arr_Respuesta['contenido'] = $arrCategorias;
+    }
+    //print_r($arrCategorias);
+    echo json_encode($arr_Respuesta);
+}
+
+require_once "../model/categoriaModel.php";
+$tipo = $_REQUEST['tipo'];
+
+//instancia de la clase ProductoModel
+$objCategoria = new ComprasModel();
+if ($tipo == "registrar") {
+    //print_r($_POST);
+    //echo $_FILES['imagen1']['name'];
+        if ($_POST) {
+        $nombre = $_POST['nombre'];
+        $detalle = $_POST['detalle'];
+        if ($nombre == "" || $detalle == "") {
+            //Vamos a responder con una estructura de objetos en formato JSON
+            //RESPUESTA
+            $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error, campos vacíos');
+        }else{
+            $arrPersona = $objCategoria->resgistrarCompras($id_producto, $cantidad, $precio, $fecha_compra, $id_persona);
+            //id es lo que me devuelve la base de datos por el procedimiento
+            if ($arrPersona->id > 0) {
+                $arr_Respuesta = array('status'=>true, 'mensaje'=>'Registro exitoso');
+            }else{
+                $arr_Respuesta = array('status'=>false, 'mensaje'=>'Error al registrar producto');
+            }
+            echo json_encode($arr_Respuesta);
+        }
+    }
+}
+
+?>
---
+++ model/comprasModel.php
@@ -0,0 +1,33 @@
+<?php
+require_once "../librerias/conexion.php";
+
+class ComprasModel
+{
+
+    private $conexion;
+    function __construct()
+    {
+        $this->conexion = new Conexion();
+        $this->conexion = $this->conexion->connect();
+    }
+    public function obtener_compras()
+    {
+        $arrRespuesta = array();
+        $respuesta = $this->conexion->query("SELECT * FROM compras");
+        while ($objeto = $respuesta->fetch_object()) {
+            array_push($arrRespuesta, $objeto);
+        }
+        return $arrRespuesta;
+    }
+    public function resgistrarCompras($id_producto, $cantidad, $precio, $fecha_compra, $id_persona)
+    {
+        $sql = $this->conexion->query("CALL registrar_categoria('{$id_producto}', '{$cantidad}', '{$precio}', '{$fecha_compra}', '{$id_persona}')");
+        //$sql = $this->conexion->query("select * from producto");
+        if ($sql === false) {
+            // Mostrar el error de la consulta
+            die("Error en la consulta: " . $this->conexion->error);
+        }
+        $sql = $sql->fetch_object();
+        return $sql;
+    }
+}
