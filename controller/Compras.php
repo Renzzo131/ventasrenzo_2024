25ab8f61771bb3f05f8729cc295ff3dc41a98337
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-12 09:58:15 -0500

    borrado bloque

--- controller/Compras.php
+++ controller/Compras.php
@@ -1,30 +1,4 @@
 <?php
-require_once "../model/categoriaModel.php";
-
-$tipo = $_REQUEST['tipo'];
-
-$objCategoria = new categoriaModel();
-
-if ($tipo == "listar"){
-    //Respuesta
-    $arr_Respuesta = array('status'=>false, 'contenido'=>'');
-    $arrCategorias = $objCategoria->obtener_categoria();
-    if (!empty($arrCategorias)){
-       //Recorremos el array para agregar las opciones de las categorias
-        for ($i=0; $i < count($arrCategorias); $i++) { 
-            $id_categoria = $arrCategorias[$i]->id;
-            $categoria = $arrCategorias[$i]->nombre;
-            $opciones = '';
-            $arrCategorias[$i]->options = $opciones;
-       } 
-       $arr_Respuesta['status'] = true;
-       $arr_Respuesta['contenido'] = $arrCategorias;
-    }
-    //print_r($arrCategorias);
-    echo json_encode($arr_Respuesta);
-}
-
-
 //instancia de la clase ProductoModel
 $objCompras = new ComprasModel();
 if ($tipo == "registrar") {
