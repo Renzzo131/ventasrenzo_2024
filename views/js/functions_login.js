759d41c77891582489384bdabca8cd9b292585e4
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-14 10:16:18 -0500

    trabajando el login

--- views/js/functions_login.js
+++ views/js/functions_login.js
@@ -1,5 +1,34 @@
 async function iniciar_sesion(){
-    console.log('payasito');
+    let usuario = document.querySelector('#usuario');
+    let password = document.querySelector('#pass');
+    if (usuario == "" || password == "") {
+        alert('Campos vacíos');
+        return;
+    }
+    try {
+        //Capturamos datos del html
+        //Estamos creando un formulario
+        const datos = new FormData(formularioLogin);
+        //Enviar datos hacia el controlador
+        //
+        let respuesta = await fetch(base_url + 'controller/login.php?tipo=iniciar_sesion', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            body: datos
+        });
+        json = await respuesta.json();
+        if (json.status) {
+            swal("Iniciar Sesión", json.mensaje, "success")
+        } else {
+            swal("Iniciar Sesión", json.mensaje, "error")
+        }
+        console.log(json);
+
+    } catch (error) {
+        console.log("Oops, ocurrió un error " + error);
+    }
+    //Los programadores suelen utilizar solo la letra e y no error.
 }
 
 if (document.querySelector('#formularioLogin')) {
--- views/login.php
+++ views/login.php
@@ -7,6 +7,9 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
+    <script>
+        const base_url = <?php echo BASE_URL;?>;
+    </script>
 </head>
 
 <body>
