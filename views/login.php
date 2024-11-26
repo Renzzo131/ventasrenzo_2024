4e1bc4f79ce3b47ecd63f2bc4d33447fa9112592
Renzzo131 <renzoeduardo1304@gmail.com>
2024-11-26 08:45:52 -0500

    base_url

--- views/login.php
+++ views/login.php
@@ -7,9 +7,10 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
-    <script>
-        const base_url = "<?php echo BASE_URL;?>";
-    </script>
+	<script>
+		//Minusculas en javascript y las mayusculas en php.
+		const base_url = '<?php echo BASE_URL;?>';
+	</script>
 </head>
 
 <body>
